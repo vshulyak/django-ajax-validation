e5b6d0487ba6a458177ae5ee6a1d7474ce8f9872
Vladimir Shulyak <vladimir@shulyak.net>
2011-11-29 14:17:57 +0400

    fixed is_callable condition to be more strict

--- ajax_validation/views.py
+++ ajax_validation/views.py
@@ -8,9 +8,11 @@ from ajax_validation.utils import LazyEncoder
 def validate(request, *args, **kwargs):
     form_class = kwargs.pop('form_class')
 
-    extra_class_args_func = kwargs.pop('class_callback', lambda request, *args, **kwargs: {}) 
-    class_kwargs = extra_class_args_func(request, *args, **kwargs)
-    form_class = form_class(**class_kwargs) if callable(form_class) else form_class
+    #this lets us pass in form factories if we're using dynamic forms
+    if callable(form_class) and not issubclass(form_class, (forms.Form, forms.ModelForm)):
+        extra_class_args_func = kwargs.pop('class_callback', lambda request, *args, **kwargs: {}) 
+        class_kwargs = extra_class_args_func(request, *args, **kwargs)
+        form_class = form_class(**class_kwargs)
 
     defaults = {
         'data': request.POST
